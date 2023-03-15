6da61d810dded0cbd187955564cd2c9f5db79985
Muskan Singh <singh722muskan@gmail.com>
2023-03-15 15:37:09 -0400

    My Profile Website

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("Script running...")
+document.querySelector('.cross').style.display = 'none';
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo')
+    if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
+        document.querySelector('.ham').style.display = 'inline'
+        document.querySelector('.cross').style.display = 'none'
+    }
+    else{
+        document.querySelector('.ham').style.display = 'none'
+        setTimeout(() => {
+            document.querySelector('.cross').style.display = 'inline'
+        }, 300); 
+    }
+})
+
+// music
+document.querySelector('#play').style.display = 'none'; 
+var x = document.getElementById("play"); 
+
+function playAudio() { 
+  x.play(); 
+} 
+
+function pauseAudio() { 
+  x.pause(); 
+} 
